9732b85aa68c79ec6f1ffa19700a87eec6f28dc0
Valentin1495 <newface95@icloud.com>
2024-01-05 15:57:28 +0900

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,12 @@ import AuthProvider from '@/components/auth-provider';
 export const metadata: Metadata = {
   title: 'AIStoryteller - Get inspired by AI',
   description: 'Generate impressive images and stories from AI',
+  icons: [
+    {
+      url: '/logo.png',
+      href: '/logo.png',
+    },
+  ],
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,39 @@
-import { BookOpenText, Image as ImageIcon } from 'lucide-react';
+import { ArrowRight, BookOpenText, Image as ImageIcon } from 'lucide-react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default async function Home() {
   return (
     <main className='flex flex-col items-center min-h-screen justify-center gap-y-8'>
       <div className='flex items-center gap-x-5'>
-        <Image src='/logo.png' alt='Logo' width={90} height={90} />
-        <h1 className='text-7xl text-center font-bold text-primary'>
+        <h1 className='text-4xl sm:text-7xl text-center font-bold text-primary'>
           AIStoryteller
         </h1>
       </div>
-      <p className='text-3xl text-primary/50 font-semibold'>
+      <p className='text-xl sm:text-3xl text-primary/50 font-semibold'>
         Experience the power of AI
       </p>
-      <div className='flex items-center gap-x-10'>
-        <Link
-          className='text-lg font-semibold text-primary flex items-center gap-x-2'
-          href='/story'
-        >
-          <article className='bg-violet-200 p-2 rounded-md'>
-            <BookOpenText className='w-5 h-5 text-violet-700' />
-          </article>
-          Story
-        </Link>
 
-        <Link
-          className='text-lg font-semibold text-primary flex items-center gap-x-2'
-          href='/thumbnail'
-        >
-          <article className='bg-rose-200 p-2 rounded-md'>
-            <ImageIcon className='w-5 h-5 text-rose-700' />
-          </article>
-          Thumbnail
-        </Link>
-      </div>
+      <Link
+        className='text-sm sm:text-lg w-64 sm:w-80 shadow-md hover:shadow-lg transition p-4 rounded-md font-semibold text-primary flex items-center gap-x-2'
+        href='/story'
+      >
+        <article className='bg-violet-200 p-2 rounded-md'>
+          <BookOpenText className='w-4 h-4 sm:w-5 sm:h-5 text-violet-700' />
+        </article>
+        Story generation
+        <ArrowRight className='w-4 h-4 sm:w-5 sm:h-5 ml-auto' />
+      </Link>
+
+      <Link
+        className='text-sm sm:text-lg w-64 sm:w-80 shadow-md hover:shadow-lg transition p-4 rounded-md font-semibold text-primary flex items-center gap-x-2'
+        href='/thumbnail'
+      >
+        <article className='bg-rose-200 p-2 rounded-md'>
+          <ImageIcon className='w-4 h-4 sm:w-5 sm:h-5 text-rose-700' />
+        </article>
+        Thumbnail generation
+        <ArrowRight className='w-4 h-4 sm:w-5 sm:h-5 ml-auto' />
+      </Link>
     </main>
   );
 }
